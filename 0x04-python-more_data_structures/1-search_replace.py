3921aa5a89e817f915988705b4157a03d667f200
Asrat1221 <agtsehay@gmail.com>
2023-05-17 05:06:45 -0700

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if search == n else n for n in my_list]
-
+    return list(map(lambda x: replace if x is search else x, my_list))
